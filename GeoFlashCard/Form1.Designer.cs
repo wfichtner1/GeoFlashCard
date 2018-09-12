3093e64de5195a7e89738964562f7d0ea16edf92
William Fichtner <william_25_35@hotmail.com>
2018-09-11 20:39:55 -0600

    images created as mineral class

--- GeoFlashCard/Form1.Designer.cs
+++ GeoFlashCard/Form1.Designer.cs
@@ -30,16 +30,16 @@
         {
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this._btnChoiceFour = new System.Windows.Forms.Button();
-            this._btnChoiceThree = new System.Windows.Forms.Button();
-            this._btnChoiceTwo = new System.Windows.Forms.Button();
-            this._btnChoiceOne = new System.Windows.Forms.Button();
-            this._btnStart = new System.Windows.Forms.Button();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
+            this._btnChoiceFour = new System.Windows.Forms.Button();
+            this._btnChoiceThree = new System.Windows.Forms.Button();
+            this._btnChoiceTwo = new System.Windows.Forms.Button();
+            this._btnChoiceOne = new System.Windows.Forms.Button();
+            this._btnStart = new System.Windows.Forms.Button();
             this.groupBox3 = new System.Windows.Forms.GroupBox();
             this._btnChoiceEight = new System.Windows.Forms.Button();
             this._btnChoiceSeven = new System.Windows.Forms.Button();
@@ -53,70 +53,30 @@
             // 
             // pictureBox1
             // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.pictureBox1.Location = new System.Drawing.Point(12, 12);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(776, 334);
+            this.pictureBox1.Size = new System.Drawing.Size(776, 443);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
             // groupBox1
             // 
+            this.groupBox1.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
             this.groupBox1.Controls.Add(this.groupBox2);
             this.groupBox1.Controls.Add(this._btnChoiceFour);
             this.groupBox1.Controls.Add(this._btnChoiceThree);
             this.groupBox1.Controls.Add(this._btnChoiceTwo);
             this.groupBox1.Controls.Add(this._btnChoiceOne);
-            this.groupBox1.Location = new System.Drawing.Point(93, 356);
+            this.groupBox1.Location = new System.Drawing.Point(93, 465);
             this.groupBox1.Name = "groupBox1";
             this.groupBox1.Size = new System.Drawing.Size(337, 142);
             this.groupBox1.TabIndex = 1;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Select the correct name";
-            // 
-            // _btnChoiceFour
-            // 
-            this._btnChoiceFour.Location = new System.Drawing.Point(196, 86);
-            this._btnChoiceFour.Name = "_btnChoiceFour";
-            this._btnChoiceFour.Size = new System.Drawing.Size(135, 23);
-            this._btnChoiceFour.TabIndex = 3;
-            this._btnChoiceFour.Text = "button4";
-            this._btnChoiceFour.UseVisualStyleBackColor = true;
-            // 
-            // _btnChoiceThree
-            // 
-            this._btnChoiceThree.Location = new System.Drawing.Point(6, 86);
-            this._btnChoiceThree.Name = "_btnChoiceThree";
-            this._btnChoiceThree.Size = new System.Drawing.Size(135, 23);
-            this._btnChoiceThree.TabIndex = 2;
-            this._btnChoiceThree.Text = "button3";
-            this._btnChoiceThree.UseVisualStyleBackColor = true;
-            // 
-            // _btnChoiceTwo
-            // 
-            this._btnChoiceTwo.Location = new System.Drawing.Point(196, 38);
-            this._btnChoiceTwo.Name = "_btnChoiceTwo";
-            this._btnChoiceTwo.Size = new System.Drawing.Size(135, 23);
-            this._btnChoiceTwo.TabIndex = 1;
-            this._btnChoiceTwo.Text = "button2";
-            this._btnChoiceTwo.UseVisualStyleBackColor = true;
-            // 
-            // _btnChoiceOne
-            // 
-            this._btnChoiceOne.Location = new System.Drawing.Point(6, 38);
-            this._btnChoiceOne.Name = "_btnChoiceOne";
-            this._btnChoiceOne.Size = new System.Drawing.Size(135, 23);
-            this._btnChoiceOne.TabIndex = 0;
-            this._btnChoiceOne.Text = "button1";
-            this._btnChoiceOne.UseVisualStyleBackColor = true;
-            // 
-            // _btnStart
-            // 
-            this._btnStart.Location = new System.Drawing.Point(12, 390);
-            this._btnStart.Name = "_btnStart";
-            this._btnStart.Size = new System.Drawing.Size(75, 23);
-            this._btnStart.TabIndex = 2;
-            this._btnStart.Text = "Begin";
-            this._btnStart.UseVisualStyleBackColor = true;
             // 
             // groupBox2
             // 
@@ -167,13 +127,61 @@
             this.button4.Text = "button1";
             this.button4.UseVisualStyleBackColor = true;
             // 
+            // _btnChoiceFour
+            // 
+            this._btnChoiceFour.Location = new System.Drawing.Point(196, 86);
+            this._btnChoiceFour.Name = "_btnChoiceFour";
+            this._btnChoiceFour.Size = new System.Drawing.Size(135, 23);
+            this._btnChoiceFour.TabIndex = 3;
+            this._btnChoiceFour.Text = "button4";
+            this._btnChoiceFour.UseVisualStyleBackColor = true;
+            // 
+            // _btnChoiceThree
+            // 
+            this._btnChoiceThree.Location = new System.Drawing.Point(6, 86);
+            this._btnChoiceThree.Name = "_btnChoiceThree";
+            this._btnChoiceThree.Size = new System.Drawing.Size(135, 23);
+            this._btnChoiceThree.TabIndex = 2;
+            this._btnChoiceThree.Text = "button3";
+            this._btnChoiceThree.UseVisualStyleBackColor = true;
+            // 
+            // _btnChoiceTwo
+            // 
+            this._btnChoiceTwo.Location = new System.Drawing.Point(196, 38);
+            this._btnChoiceTwo.Name = "_btnChoiceTwo";
+            this._btnChoiceTwo.Size = new System.Drawing.Size(135, 23);
+            this._btnChoiceTwo.TabIndex = 1;
+            this._btnChoiceTwo.Text = "button2";
+            this._btnChoiceTwo.UseVisualStyleBackColor = true;
+            // 
+            // _btnChoiceOne
+            // 
+            this._btnChoiceOne.Location = new System.Drawing.Point(6, 38);
+            this._btnChoiceOne.Name = "_btnChoiceOne";
+            this._btnChoiceOne.Size = new System.Drawing.Size(135, 23);
+            this._btnChoiceOne.TabIndex = 0;
+            this._btnChoiceOne.Text = "button1";
+            this._btnChoiceOne.UseVisualStyleBackColor = true;
+            // 
+            // _btnStart
+            // 
+            this._btnStart.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this._btnStart.Location = new System.Drawing.Point(12, 499);
+            this._btnStart.Name = "_btnStart";
+            this._btnStart.Size = new System.Drawing.Size(75, 23);
+            this._btnStart.TabIndex = 2;
+            this._btnStart.Text = "Begin";
+            this._btnStart.UseVisualStyleBackColor = true;
+            this._btnStart.Click += new System.EventHandler(this._btnStart_Click);
+            // 
             // groupBox3
             // 
+            this.groupBox3.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
             this.groupBox3.Controls.Add(this._btnChoiceEight);
             this.groupBox3.Controls.Add(this._btnChoiceSeven);
             this.groupBox3.Controls.Add(this._btnChoiceSix);
             this.groupBox3.Controls.Add(this._btnChoiceFive);
-            this.groupBox3.Location = new System.Drawing.Point(451, 356);
+            this.groupBox3.Location = new System.Drawing.Point(451, 465);
             this.groupBox3.Name = "groupBox3";
             this.groupBox3.Size = new System.Drawing.Size(337, 142);
             this.groupBox3.TabIndex = 3;
@@ -182,6 +190,7 @@
             // 
             // _btnChoiceEight
             // 
+            this._btnChoiceEight.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
             this._btnChoiceEight.Location = new System.Drawing.Point(196, 86);
             this._btnChoiceEight.Name = "_btnChoiceEight";
             this._btnChoiceEight.Size = new System.Drawing.Size(135, 23);
@@ -191,6 +200,7 @@
             // 
             // _btnChoiceSeven
             // 
+            this._btnChoiceSeven.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
             this._btnChoiceSeven.Location = new System.Drawing.Point(6, 86);
             this._btnChoiceSeven.Name = "_btnChoiceSeven";
             this._btnChoiceSeven.Size = new System.Drawing.Size(135, 23);
@@ -200,6 +210,7 @@
             // 
             // _btnChoiceSix
             // 
+            this._btnChoiceSix.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
             this._btnChoiceSix.Location = new System.Drawing.Point(196, 38);
             this._btnChoiceSix.Name = "_btnChoiceSix";
             this._btnChoiceSix.Size = new System.Drawing.Size(135, 23);
@@ -209,6 +220,7 @@
             // 
             // _btnChoiceFive
             // 
+            this._btnChoiceFive.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
             this._btnChoiceFive.Location = new System.Drawing.Point(6, 38);
             this._btnChoiceFive.Name = "_btnChoiceFive";
             this._btnChoiceFive.Size = new System.Drawing.Size(135, 23);
@@ -220,11 +232,13 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 510);
+            this.ClientSize = new System.Drawing.Size(800, 619);
             this.Controls.Add(this.groupBox3);
             this.Controls.Add(this._btnStart);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
             this.Name = "TriviaForm";
             this.Text = "Mineral Trivia";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
--- GeoFlashCard/Form1.cs
+++ GeoFlashCard/Form1.cs
@@ -1,9 +1,13 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Globalization;
 using System.Linq;
+using System.Reflection;
+using System.Resources;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,12 +16,30 @@ namespace GeoFlashCard
 {
     public partial class TriviaForm : Form
     {
-        Dictionary<(string, string), Bitmap> mineralDictionary = new Dictionary<(string, string), Bitmap>();
+        List<Mineral> mineralList = new List<Mineral>();
+        Random rand = new Random();
+        
         public TriviaForm()
         {
-            InitializeComponent();            
-            Mineral handMineral = new Mineral("hand", "AlMgDe", GeoFlashCard.Properties.Resources.handImage);
-            pictureBox1.Image = handMineral._mineralImage;
+            InitializeComponent();
+            //Iterates through each member of the application resources class. Returned as property
+            foreach(PropertyInfo property in typeof(Properties.Resources).GetProperties(BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic))
+            {
+                if (!(property.Name.Equals("ResourceManager") || property.Name.Equals("Culture")))
+                {
+                    Bitmap original = (Bitmap)property.GetValue(property.Name);
+                    Bitmap resized = new Bitmap(original, new Size(600, 480));
+                    mineralList.Add(new Mineral(property.Name, "test", resized));
+                }                                    
+            }
+
+            pictureBox1.Image = mineralList[0]._mineralImage;
+        }
+
+        private void _btnStart_Click(object sender, EventArgs e)
+        {
+            int index = rand.Next(0, mineralList.Count - 1);
+            pictureBox1.Image = mineralList[index]._mineralImage;
         }
     }
 }
